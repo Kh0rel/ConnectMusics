3f33a119d23dc7fe72c458571d9e406fb4285cb8
Guillaume Chieb Bouares <guillaume@horsecom.io>
2017-01-31 20:34:33 +0100

    init project

---
+++ ConnectMusics/ConnectMusics/ConnectMusics.h
@@ -0,0 +1,19 @@
+//
+//  ConnectMusics.h
+//  ConnectMusics
+//
+//  Created by guillaume chieb bouares on 31/01/2017.
+//  Copyright © 2017 com.github.shemana.application. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ConnectMusics.
+FOUNDATION_EXPORT double ConnectMusicsVersionNumber;
+
+//! Project version string for ConnectMusics.
+FOUNDATION_EXPORT const unsigned char ConnectMusicsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConnectMusics/PublicHeader.h>
+
+

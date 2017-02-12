53e5cbaf9a714ae77b8d9f331eb2b6e68b048855
Guillaume Chieb Bouares <guillaume@horsecom.io>
2017-02-12 18:02:41 +0100

    CMSharedProvidersDocumentation

--- ConnectMusics/CMSharedProviders.swift
+++ ConnectMusics/CMSharedProviders.swift
@@ -9,15 +9,30 @@
 
 
 class CMSharedProviders: NSObject {
-
+    /*!
+     * @brief Singleton instance for Providers Handler
+     */
     internal static let sharedInstance = CMSharedProviders()
-    
+    /*!
+     * @brief all playlists retreived from WebService
+     */
     private var playlists:[CMPlaylist] = []
-    
+    /*!
+     * @brief all providersIsnstance create by application
+     */
     private var providersInstance:[CMBaseProvider] = []
     
     //TODO UNIT TEST CREATE A NEW PROVIDER 
     //TODO UNIT TEST 2 test second creation of the same provider type -> return error
+    
+    /*!
+     * @discussion method which switch conditon for separate specific ProviderType instructions
+     * @param provider choose which provider you want to create
+     * @param clientID set it if an authentification by Oauth2 is needed
+     * @param clientSecret like ClientID this param is required if you need an Oauth2 authentication
+     * @param redirectURI it's your URIScheme for retrieve access_token if your provider need one URI
+     * @param scopeNeeded it's a permission scheme needed for token creation , it's different for any providers
+     */
     internal func addProviders(provider:ProviderType,clientID:String? = nil,clientSecret:String? = nil,redirectURI:String? = nil,scopeNeeded:String? = nil) {
         //Condition If an provider already exist
         switch provider {
@@ -29,7 +44,11 @@ class CMSharedProviders: NSObject {
             break
         }
     }
-    
+    /*!
+     * @discussion method which allow access to specific provider already instanciate
+     * @param wantedProvider filter parameter
+     * @return Return Provider instance if is exists, else return nil
+     */
     public func getProvider(wantedProvider:ProviderType) -> CMBaseProvider? {
         for provider in providersInstance {
             if provider.type == wantedProvider {

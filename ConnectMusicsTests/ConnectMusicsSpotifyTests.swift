bed57299ada637c6052505a01585324c96b34799
Guillaume Chieb Bouares <guillaume@horsecom.io>
2017-02-15 23:01:00 +0100

    init unit test

--- ConnectMusicsTests/ConnectMusicsSpotifyTests.swift
+++ ConnectMusicsTests/ConnectMusicsSpotifyTests.swift
@@ -15,14 +15,9 @@ class ConnectMusicsSpotifyTests: XCTestCase {
 
     override func setUp() {
         super.setUp()
-        
-        /*let path = Bundle.main.url(forResource: "clientInformations", withExtension: "json")
-        let data = NSData(contentsOf: path!)
-        let json = JSON(data: data as! Data)
-        clientInformations["access_token"] = json["access_token"].string!
-        clientInformations["refresh_token"] = json["refresh_token"].string!
-        */
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        clientInformations["access_token"] = "QCLptMvzliGZfrNnfb8OnaS55KV3BPj9s8wjuPQyJHFHMc0ew7WM1Ph65st-1_twvkXKRxsdMfpd8UbBKWik92CC5kF3lh4maRYHpwIERQtwU6JqndTHiTfdlWiSUYKcoQH3AXssBW2AItwB6LjrAswGB8afiDrNMoZYvCh64fYbKBaxA-8QWN7F6IOGlMoWOX8zXhn"
+        clientInformations["refresh_token"] = "AQBxffMlLhk9onQ1GU52ju4-H1-ijysbFwl0QMgP8R7V0vFvRqnI1buztkWkeU6tnmS30B05COCLusf1ZJ-YYqaT7uQQ4GoEH1r9E5UwW31WE6L9zinFwh3Ugd2tKluEKC8"
+        clientInformations["user_id"] = "tictakitkat"
     }
     
     override func tearDown() {
@@ -31,6 +26,7 @@ class ConnectMusicsSpotifyTests: XCTestCase {
     }
     
     func testGetMe() {
+        
         
     }
     
